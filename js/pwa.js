4c771fd82ab56091f2140da82cafc2d411356a30
haddadiamirhosein <haddadiamirhosein.d@gmail.com>
2022-07-17 17:57:39 +0430

    Site template

---
+++ js/pwa.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/serviceWorker.js")
+      .then(reg => {
+        console.log("Service worker registred successfully", reg);
+      })
+      .catch(err => {
+        console.log("service worker not registred !!", err);
+      });
+  }
